fb7a21075e1ee02b19df7bc7005728e57677d405
Akbarali Salohiddinov <akbaralisalohiddinov808@gmail.com>
2025-04-17 09:39:46 +0500

    create serializer for api

--- api/serializers.py
+++ api/serializers.py
@@ -5,3 +5,4 @@ from apps.users.validators import validate_phone_number
 
 class UserAuthCodeSerializer(serializers.Serializer):
     phone_number = serializers.CharField(max_length=20, validators=[validate_phone_number])
+    
